7d6b44b5afd8abbce9e2f8446140ecd13c7ce86e
BELVEDERE <belvederedriving@gmail.com>
2026-05-15 08:03:44 +0200

    delete vite module

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,8 @@
 import type { NextConfig } from 'next';
-const nextConfig: NextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] }, experimental: { staleTimes: { dynamic: 30, static: 180 } } };
+import path from 'path';
+const nextConfig: NextConfig = {
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+  experimental: { staleTimes: { dynamic: 30, static: 180 } },
+  turbopack: { root: path.resolve(__dirname) },
+};
 export default nextConfig;
